0a61cd50afe2970dbcef0590b13eb35bc30e221d
Ujjwal-027 <bhattaraiujwal09@gmail.com>
2025-07-06 22:14:21 +0545

    Doctors contd

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -124,6 +124,192 @@ const Doctors = () => {
           </div> 
         </div>
       </div>
+
+      <div>
+        <div>
+          {specialities.map((spec, i) => (
+            <p key={i} onClick={() => navigate(`/doctors/${spec}`)} className="flex bg-slate-500 item-center justify-between border-blue-200 rounded-xl cursor-pointer hover:translate-y-[-10px] transition-all duration-500">
+              {spec}
+            </p>
+          ))}
+        </div>
+        <div className='flex flex-col items-center gap-4 my-16 text-gray-90 md:mx-10 bg-red-800'>
+          <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
+            {
+              filterDoc.map((item, index)=>(
+                <div onClick={()=>navigate(`/appointment/${item._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
+                  <img className='bg-green-50' src={item.image} alt="" /> 
+                  <div className='p-4'>
+                    <div className='flex item-center gap-2 text-sm text-center text-green-500'>
+                        <p className='w-2 h-2 bg-green-500 rounded-full'></p><p>Available</p>
+                    </div>
+                    <p className='text-gray-900 text-lg font-medium'>{item.name}</p>
+                    <p className='text-gray-900 text-sm'>{item.speciality}</p>
+                  </div>
+                  
+
+                  
+                </div>
+              ))
+            }
+          </div> 
+        </div>
+      </div>
+
+      <div>
+        <div>
+          {specialities.map((spec, i) => (
+            <p key={i} onClick={() => navigate(`/doctors/${spec}`)} className="flex bg-slate-500 item-center justify-between border-blue-200 rounded-xl cursor-pointer hover:translate-y-[-10px] transition-all duration-500">
+              {spec}
+            </p>
+          ))}
+        </div>
+        <div className='flex flex-col items-center gap-4 my-16 text-gray-90 md:mx-10 bg-red-800'>
+          <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
+            {
+              filterDoc.map((item, index)=>(
+                <div onClick={()=>navigate(`/appointment/${item._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
+                  <img className='bg-green-50' src={item.image} alt="" /> 
+                  <div className='p-4'>
+                    <div className='flex item-center gap-2 text-sm text-center text-green-500'>
+                        <p className='w-2 h-2 bg-green-500 rounded-full'></p><p>Available</p>
+                    </div>
+                    <p className='text-gray-900 text-lg font-medium'>{item.name}</p>
+                    <p className='text-gray-900 text-sm'>{item.speciality}</p>
+                  </div>
+                  
+
+                  
+                </div>
+              ))
+            }
+          </div> 
+        </div>
+      </div>
+
+      <div>
+        <div>
+          {specialities.map((spec, i) => (
+            <p key={i} onClick={() => navigate(`/doctors/${spec}`)} className="flex bg-slate-500 item-center justify-between border-blue-200 rounded-xl cursor-pointer hover:translate-y-[-10px] transition-all duration-500">
+              {spec}
+            </p>
+          ))}
+        </div>
+        <div className='flex flex-col items-center gap-4 my-16 text-gray-90 md:mx-10 bg-red-800'>
+          <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
+            {
+              filterDoc.map((item, index)=>(
+                <div onClick={()=>navigate(`/appointment/${item._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
+                  <img className='bg-green-50' src={item.image} alt="" /> 
+                  <div className='p-4'>
+                    <div className='flex item-center gap-2 text-sm text-center text-green-500'>
+                        <p className='w-2 h-2 bg-green-500 rounded-full'></p><p>Available</p>
+                    </div>
+                    <p className='text-gray-900 text-lg font-medium'>{item.name}</p>
+                    <p className='text-gray-900 text-sm'>{item.speciality}</p>
+                  </div>
+                  
+
+                  
+                </div>
+              ))
+            }
+          </div> 
+        </div>
+      </div>
+
+      <div>
+        <div>
+          {specialities.map((spec, i) => (
+            <p key={i} onClick={() => navigate(`/doctors/${spec}`)} className="flex bg-slate-500 item-center justify-between border-blue-200 rounded-xl cursor-pointer hover:translate-y-[-10px] transition-all duration-500">
+              {spec}
+            </p>
+          ))}
+        </div>
+        <div className='flex flex-col items-center gap-4 my-16 text-gray-90 md:mx-10 bg-red-800'>
+          <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
+            {
+              filterDoc.map((item, index)=>(
+                <div onClick={()=>navigate(`/appointment/${item._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
+                  <img className='bg-green-50' src={item.image} alt="" /> 
+                  <div className='p-4'>
+                    <div className='flex item-center gap-2 text-sm text-center text-green-500'>
+                        <p className='w-2 h-2 bg-green-500 rounded-full'></p><p>Available</p>
+                    </div>
+                    <p className='text-gray-900 text-lg font-medium'>{item.name}</p>
+                    <p className='text-gray-900 text-sm'>{item.speciality}</p>
+                  </div>
+                  
+
+                  
+                </div>
+              ))
+            }
+          </div> 
+        </div>
+      </div>
+
+      <div>
+        <div>
+          {specialities.map((spec, i) => (
+            <p key={i} onClick={() => navigate(`/doctors/${spec}`)} className="flex bg-slate-500 item-center justify-between border-blue-200 rounded-xl cursor-pointer hover:translate-y-[-10px] transition-all duration-500">
+              {spec}
+            </p>
+          ))}
+        </div>
+        <div className='flex flex-col items-center gap-4 my-16 text-gray-90 md:mx-10 bg-red-800'>
+          <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
+            {
+              filterDoc.map((item, index)=>(
+                <div onClick={()=>navigate(`/appointment/${item._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
+                  <img className='bg-green-50' src={item.image} alt="" /> 
+                  <div className='p-4'>
+                    <div className='flex item-center gap-2 text-sm text-center text-green-500'>
+                        <p className='w-2 h-2 bg-green-500 rounded-full'></p><p>Available</p>
+                    </div>
+                    <p className='text-gray-900 text-lg font-medium'>{item.name}</p>
+                    <p className='text-gray-900 text-sm'>{item.speciality}</p>
+                  </div>
+                  
+
+                  
+                </div>
+              ))
+            }
+          </div> 
+        </div>
+      </div>
+
+      <div>
+        <div>
+          {specialities.map((spec, i) => (
+            <p key={i} onClick={() => navigate(`/doctors/${spec}`)} className="flex bg-slate-500 item-center justify-between border-blue-200 rounded-xl cursor-pointer hover:translate-y-[-10px] transition-all duration-500">
+              {spec}
+            </p>
+          ))}
+        </div>
+        <div className='flex flex-col items-center gap-4 my-16 text-gray-90 md:mx-10 bg-red-800'>
+          <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
+            {
+              filterDoc.map((item, index)=>(
+                <div onClick={()=>navigate(`/appointment/${item._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={index}>
+                  <img className='bg-green-50' src={item.image} alt="" /> 
+                  <div className='p-4'>
+                    <div className='flex item-center gap-2 text-sm text-center text-green-500'>
+                        <p className='w-2 h-2 bg-green-500 rounded-full'></p><p>Available</p>
+                    </div>
+                    <p className='text-gray-900 text-lg font-medium'>{item.name}</p>
+                    <p className='text-gray-900 text-sm'>{item.speciality}</p>
+                  </div>
+                  
+
+                  
+                </div>
+              ))
+            }
+          </div> 
+        </div>
+      </div>
             
     </div>
   )
